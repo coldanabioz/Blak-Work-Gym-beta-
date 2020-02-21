6ec72743a48bcbd6c9b09d5082e32e35e661d54a
unknown <coldanabioz@gmail.com>
2020-02-21 16:43:18 +0200

    Comment

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+$('.gallSlidCont').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    centerMode: true,
+    centerPaddings: '25px',
+    dots: true,
+    slidesToShow: 2,
+});
